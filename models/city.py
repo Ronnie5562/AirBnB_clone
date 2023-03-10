307a1a520ff1317a634af0ed9775b41f4c8dd762
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-03-10 22:57:23 +0200

    city.py

--- models/city.py
+++ models/city.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """_summary_
-    This module creates new instances of the Amenity class -- This is visible in the frontend of this project
+    This module creates new instances of the City class -- This is visible in the frontend of this project
 """
 from models.base_model import BaseModel
 
-class Amenity(BaseModel):
+
+class City(BaseModel):
     """_summary_
         This class inherits from a super class named - BaseModel
         name(str): empty string
+        state_id(str): empty string
     """
+    state_id = ""
     name = ""
 
     def __init__(self, *args, **kwargs):
