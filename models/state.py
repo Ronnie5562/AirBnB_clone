5dcd01dae14dfa350421e97c4d8ec14b22281fb8
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-03-10 23:20:41 +0200

    state.py

---
+++ models/state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""_summary_
+    This module creates new instances of the State class -- This is visible in the frontend of this project
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """_summary_
+        This class inherits from a super class named - BaseModel
+        name(str): empty string
+    """
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
