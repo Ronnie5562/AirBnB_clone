7153de82eaf8d7bdccfc50c04fc08671e0790edd
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-03-10 23:17:18 +0200

    review.py

---
+++ models/review.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""_summary_
+    This module creates new instances of the Review class -- This is visible in the frontend of this project
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """_summary_
+        This class inherits from a super class named - BaseModel
+        place_id(str): empty string
+        user_id(str): empty string
+        text(str): empty string
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
