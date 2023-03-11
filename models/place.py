8f48f4fbebc7a3f3781417dd7fdbf682d7c171f6
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-03-11 13:52:50 +0200

    place.py

--- models/place.py
+++ models/place.py
@@ -34,4 +34,7 @@ class Place(BaseModel):
     amenity_ids = []
 
     def __init__(self, *args, **kwargs):
+        """_summary_
+            This constructor method inherits from the constructor method in BaseModel
+        """
         super().__init__(*args, **kwargs)
