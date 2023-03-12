2497a7bd222f5846aa21aab7a6319a59c06c27fc
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-03-13 00:15:20 +0200

    created a test

---
+++ tests/test_console.py
@@ -0,0 +1,42 @@
+'''
+    module documentation
+'''
+import unittest
+from models.base_model import BaseModel
+import os
+import sys
+from console import HBNBCommand
+from io import StringIO
+
+
+class TestHBNBCommand(unittest.TestCase):
+    """_summary_
+        This class test HBNBCommand class.
+    """
+
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def out_test(self, func, arg, expect):
+        """Axiliar function to test some commands of the console"""
+        std_out = StringIO()
+        sys.stdout = std_out
+        func(arg)
+        output = std_out.getvalue()
+        self.assertEqual(output, expect + '\n')
+        return output
+
+    def test_creation_failed(self):
+        """Testing the 'create' command of the console - the error messages"""
+        try:
+            os.remove('file.json')
+        except:
+            pass
+        cmd = HBNBCommand()
+
+        self.out_test(cmd.do_create, '', HBNBCommand.ERROR_CLASS_NAME)
+        self.out_test(cmd.do_create, 'myModel', HBNBCommand.ERROR_CLASS)
+
+    # def test_create_ok(self):
+    #     cmd = HBNBCommand()
+    #     out = self.out_test(cmd.do_create, 'BaseModel', '')
