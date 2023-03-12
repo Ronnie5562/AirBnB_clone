903dc8d4b2cc6f65ae5ecc7c9ecc26ba95f75cac
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-03-12 23:51:43 +0200

    created a test

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""_summary_
+    This module test the review module present in models
+"""
+import unittest
+from models.review import Review
+
+
+class TestReview(unittest.TestCase):
+    """_summary_
+        This class test user class.
+    """
+
+    def setUp(self):
+        self.review = Review()
+
+    def test_creation(self):
+        """ 
+            This test validate that creation proccess was correct.
+        """
+        self.assertEqual(self.review.text, '')
